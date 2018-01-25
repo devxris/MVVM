56fa72894a422dbbbc9cceaaaf6f4320dfe7aa2a
devxris <chuwei0915@gmail.com>
2018-01-25 15:41:14 +0800

    MVVM - initial setup

---
+++ MVVM/Controller/RegistrationTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  RegistrationTableViewController.swift
+//  MVVM
+//
+//  Created by Chris Huang on 25/01/2018.
+//  Copyright © 2018 Chris Huang. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationTableViewController: UITableViewController {
+
+}
